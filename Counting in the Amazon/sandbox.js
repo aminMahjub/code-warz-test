fb40cf186acdb495fb6b091b24c35562733f792f
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-12-17 11:30:01 +0330

    Optimize counting in the amazon code

--- Counting in the Amazon/sandbox.js
+++ Counting in the Amazon/sandbox.js
@@ -1,31 +1,16 @@
 const countArara = n => {
-    const anane = 1;
-    const adak = 2;
+    let number = n % 2 === 0 ? n / 2 : null || n % 2 === 1 ? (n - 1) / 2 : null;
+    let string = 'adak ';    
 
-    // let result = n % 2 === 0 ? n / 2 : null || n % 2 === 1 ? (n - 1) / 2 : null;
     n === 1 ? console.log('anane') : null;
     n === 2 ? console.log('adak') : null;
 
-    if (n % 2 === 0) {
-        result = n / 2;
-        let string = 'adak '
-
-        for (let i = 0; i < result - 1; i++) {
-            string += 'adak ';
-        }
-
-        console.log(string);
-    } else {
-        result = (n - 1) / 2;
-        let string = 'adak ';
-
-        for (let i = 0; i < result - 1; i++) {
-            string += 'adak ';
-        }
-
-        string += 'anane';
-        console.log(string);
+    for (let i = 0; i < number - 1; i++) {
+        string += 'adak ';
     }
+
+    if (n % 2 === 1) string += 'anane';
+    console.log(string);
 }
 
-countArara(8)
+countArara(7)
